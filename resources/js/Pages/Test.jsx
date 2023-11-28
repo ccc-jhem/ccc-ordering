2e8c7868b3bd52bb651ab6e79b43cf93cd8477bc
Jhemar Danio <jdanio@chiccentre.local>
2023-11-28 14:08:18 +0800

    test commit

--- resources/js/Pages/Test.jsx
+++ resources/js/Pages/Test.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 const Test = () => {
     return (
-        <h1>This is test component</h1>
+        <h1>This is a test component and push to development branch</h1>
     )
 }
 
