e5979b5c69f7a9236c2fa3a4e859977b02833e11
Jhemar Danio <jdanio@chiccentre.local>
2023-11-30 17:35:53 +0800

    fix error class to className

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -10,75 +10,75 @@ export default function Dashboard({ auth }) {
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-4 sm:p-6 text-gray-900">
                             <div className="relative overflow-x-auto">
-                                <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                    <thead class="text-xs text-white uppercase bg-black dark:bg-gray-700 dark:text-gray-400">
+                                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                    <thead className="text-xs text-white uppercase bg-black dark:bg-gray-700 dark:text-gray-400">
                                         <tr>
-                                            <th scope="col" class="px-6 py-3">
+                                            <th scope="col" className="px-6 py-3">
                                                 SKU NAME
                                             </th>
-                                            <th scope="col" class="px-6 py-3">
+                                            <th scope="col" className="px-6 py-3">
                                                 DESCRIPTION
                                             </th>
-                                            <th scope="col" class="px-6 py-3">
+                                            <th scope="col" className="px-6 py-3">
                                                 Category
                                             </th>
-                                            <th scope="col" class="px-6 py-3">
+                                            <th scope="col" className="px-6 py-3">
                                                 PRICE
                                             </th>
-                                            <th scope="col" class="px-6 py-3">
+                                            <th scope="col" className="px-6 py-3">
                                                 QTY
                                             </th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                                 Apple MacBook Pro 17"
                                             </th>
-                                            <td class="px-6 py-4">
+                                            <td className="px-6 py-4">
                                                 Silver
                                             </td>
-                                            <td class="px-6 py-4">
+                                            <td className="px-6 py-4">
                                                 Laptop
                                             </td>
-                                            <td class="px-6 py-4">
+                                            <td className="px-6 py-4">
                                                 $2999
                                             </td>
-                                            <td class="px-6 py-4">
+                                            <td className="px-6 py-4">
                                               3
                                             </td>
                                         </tr>
-                                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                                 Microsoft Surface Pro
                                             </th>
-                                            <td class="px-6 py-4">
+                                            <td className="px-6 py-4">
                                                 White
                                             </td>
-                                            <td class="px-6 py-4">
+                                            <td className="px-6 py-4">
                                                 Laptop PC
                                             </td>
-                                            <td class="px-6 py-4">
+                                            <td className="px-6 py-4">
                                                 $1999
                                             </td>
-                                            <td class="px-6 py-4">
+                                            <td className="px-6 py-4">
                                               3
                                             </td>
                                         </tr>
-                                        <tr class="bg-white dark:bg-gray-800">
-                                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        <tr className="bg-white dark:bg-gray-800">
+                                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                                 Magic Mouse 2
                                             </th>
-                                            <td class="px-6 py-4">
+                                            <td className="px-6 py-4">
                                                 Black
                                             </td>
-                                            <td class="px-6 py-4">
+                                            <td className="px-6 py-4">
                                                 Accessories
                                             </td>
-                                            <td class="px-6 py-4">
+                                            <td className="px-6 py-4">
                                                 $99
                                             </td>
-                                            <td class="px-6 py-4">
+                                            <td className="px-6 py-4">
                                               3
                                             </td>
                                         </tr>
